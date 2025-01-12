e96526bd70f178fcdcc732fd4f9b96707cc12be1
Dom Clayton <dominic@booandjoel.co.uk>
2025-01-12 20:58:11 +0000

    Added tests, and comments for how to run them

---
+++ tests/regression_test.go
@@ -0,0 +1,35 @@
+package test
+
+import (
+	
+)
+
+
+
+func TestItApplies_Regression_Part_1(t *testing.T) {
+	t.Parallel()
+
+	opts := GetTestConfig(t)
+
+	terraformOptions := Setup(t, "examples/standalone", opts)
+
+	_, err := terraform.InitAndApplyE(t, terraformOptions)
+	require.Nil(t, err)
+}
+
+func TestItApplies_Regression_Part_2(t *testing.T) {
+	t.Parallel()
+
+	opts := GetTestConfig(t)
+
+	terraformOptions := Setup(t, "examples/standalone", opts)
+
+	defer terraform.Destroy(t, terraformOptions)
+	applyOutput, err := terraform.ApplyAndIdempotent(t, terraformOptions)
+
+	// Check if apply output contains indications of resource changes
+	hasChanges := strings.Contains(applyOutput, "Apply complete!")
+	require.Nil(t, err)
+	//require.Equal(t, "", "")
+	assert.True(t, hasChanges, "Terraform apply made no changes.")
+}
